6b9edbdbe8f9ed1c31b56fc5068102e362ccaae1
bis <132788819+1bisgit@users.noreply.github.com>
2023-05-26 22:41:20 -0300

    Add files via upload

---
+++ amsterdaip.py
@@ -0,0 +1,35 @@
+logo=f'''
+                         _               _       
+     /\                 | |             | |      
+    /  \   _ __ ___  ___| |_ ___ _ __ __| | __ _ 
+   / /\ \ | '_ ` _ \/ __| __/ _ \ '__/ _` |/ _` |
+  / ____ \| | | | | \__ \ ||  __/ | | (_| | (_| |
+ /_/    \_\_| |_| |_|___/\__\___|_|  \__,_|\__,_|
+                                                 
+                                                 
+'''
+def encurtarLink(long_url):
+
+algoritmo para encurtar link aqui
+    return short_url
+
+def getIP(url):
+    # função para obter ip aqui
+
+    return ip
+
+def analiseIP(ip):
+    # função para analisar ip aqui
+
+    return informacoesIP
+
+long_url = input('Insira seu link aqui: ')
+
+short_url = encurtarLink(long_url)
+print('Seu link encurtado é:', short_url)
+
+ip = getIP(short_url)
+print('IP:', ip)
+
+informacoesIP = analiseIP(ip)
+print('Informações sobre o IP:', informacoesIP)
